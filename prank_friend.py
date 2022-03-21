e9205f2edb484b40bedc0e507eadfc40508ecc75
Ishankhan01 <98836294+Ishankhan01@users.noreply.github.com>
2022-03-22 00:26:59 +0530

    Update prank_friend.py

--- prank_friend.py
+++ prank_friend.py
@@ -1,26 +1,15 @@
-#create digital clock using python
-#code by SecretBeast_bg01
+#prank with your friend with this program
+#Code by SecretBeast_bg01
 
-
-from tkinter import Label, Tk 
+import random
+import pyautogui as pg
 import time
-app_window = Tk() 
-app_window.title("Digital Clock") 
-app_window.geometry("420x150") 
-app_window.resizable(1,1)
 
-text_font= ("Boulder", 68, 'bold')
-background = "red"
-foreground= "#363529"
-border_width = 25
+foryourfriend =('son of a bit**','mother fu***','sister fu**','scu***k','chutiya')
 
-label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
-label.grid(row=0, column=1)
+time.sleep(10)
+for i in range(2000):
+    a = random.choice(foryourfriend)
+    pg.write('you '+a)
+    pg.press('enter')
 
-def digital_clock(): 
-   time_live = time.strftime("%H:%M:%S")
-   label.config(text=time_live) 
-   label.after(200, digital_clock)
-
-digital_clock()
-app_window.mainloop()
